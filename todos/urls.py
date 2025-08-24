a8ff98f7e06336c6aed86b89d35e00b522e06425
Amay Thakur <thakuramay007@gmail.com>
2025-08-24 15:31:47 +0530

    Delete opertaion added

--- todos/urls.py
+++ todos/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('addTask/' , views.addTask, name= 'addTask')
+    path('addTask/' , views.addTask, name= 'addTask'),
+    path('mark_as_done/<int:pk>', views.markedDone, name ='mark_as_done'),
+    path('mark_as_undone/<int:pk>', views.markUndone , name ='mark_as_undone'),
+    path('delete_task/<int:pk>', views.delete_task , name ='delete_task'),
+
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from .models import Task
 
 def addTask(request):
-    task= request.POST['task']
+    task = request.POST['task']
     Task.objects.create(task = task)
     return redirect('home')
 
+def markedDone(request , pk):
+    task= get_object_or_404(Task , pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def markUndone(request , pk):
+    task= get_object_or_404(Task , pk=pk)
+    task.is_completed = False 
+    task.save()
+    return redirect('home')
+
+def delete_task(request, pk):
+    task = get_object_or_404 (Task , pk=pk)
+    task.delete()
+    return redirect('home')
 
 
     
