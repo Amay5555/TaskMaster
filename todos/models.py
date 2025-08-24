064dcdd83d5ee093b5175d4a644ffe7a1166c6c0
Amay Thakur <thakuramay007@gmail.com>
2025-08-24 14:08:53 +0530

    Django app created

---
+++ todos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Task(models.Model):
+    task = models.CharField(null = False , blank = False , max_length = 200)
+    is_completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField (auto_now_add= True)
+    updated_at = models.DateTimeField (auto_now= True)
+
+    def __str__(self):
+        return self.task
+    
+
+
---
+++ todos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+from .models import Task
+
+def addTask(request):
+    task= request.POST['task']
+    Task.objects.create(task = task)
+    return redirect('home')
+
+
+
+    
